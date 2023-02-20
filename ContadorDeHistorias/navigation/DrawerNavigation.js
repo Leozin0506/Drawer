3f9cd77c752070847a7704e424c412a797b87dd1
Leozin0506 <94076803+Leozin0506@users.noreply.github.com>
2023-02-20 10:16:58 -0300

    Add files via upload

---
+++ ContadorDeHistorias/navigation/DrawerNavigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import BottomTabNavigator from '../navigation/TabNavigator';
+import Profile from '../screens/Profile';
+
+const Drawer = createDrawerNavigator();
+const DrawerNavigator =() =>{
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen name="Tela Inicial" component= {BottomTabNavigator}/>
+            <Drawer.Screen name="Perfil" component= {Profile}/>
+        </Drawer.Navigator>
+    )
+}
+export default DrawerNavigator;
